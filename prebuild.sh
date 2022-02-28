e084315c77dec4aad43f74b71c4aa8aa09d4cefd
yunan777 <yunan_wang@outlook.com>
2022-02-28 20:34:24 +0800

    create repo

---
+++ prebuild.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd "$(dirname $0)"
+
+source .env
+
+docker run --rm httpd:2.4-alpine htpasswd -nbB ${TRAEFIK_DASHBORD_USER} ${TRAEFIK_DASHBORD_PSWD} > ./config/traefik/password
+
+chmod 600 ./config/traefik/password
+
+echo -n "${PORTAINER_PSWD}" > ./config/portainer/password
+
+chmod 600 ./config/portainer/password
+
+touch ./config/traefik/acme.json && chmod 600 ./config/traefik/acme.json
+
+docker-compose build nginx php8.0-fpm mysql traefik redis workspace portainer adminer
+
+echo -e "Complete.\nRun 'docker-compose up' to start service.\nRun 'docker-compose run workspace bash' to get in develop environment."
