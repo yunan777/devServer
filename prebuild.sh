508fabb30fbe6b723a960a78b00c010b45ef2897
yunan777 <yunan_wang@outlook.com>
2022-03-01 14:00:23 +0800

    bugfix

--- prebuild.sh
+++ prebuild.sh
@@ -12,10 +12,11 @@ echo -n "${PORTAINER_PSWD}" > ./config/portainer/password
 
 chmod 600 ./config/portainer/password
 
-touch ./config/traefik/acme.json && chmod 600 ./config/traefik/acme.json
+touch ./config/traefik/acme_staging.json && chmod 600 ./config/traefik/acme_staging.json
+touch ./config/traefik/acme_production.json && chmod 600 ./config/traefik/acme_production.json
 
 docker-compose build php8.0-fpm workspace
 
 docker-compose pull nginx mysql traefik redis portainer adminer
 
-echo -e "Complete.\nRun 'docker-compose up' to start service.\nRun 'docker-compose run workspace bash' to get in develop environment."
+echo -e "Complete.\nRun 'docker-compose up -d' to start service.\nRun 'docker-compose run workspace bash' to get in develop environment."
